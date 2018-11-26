f7857e8b550bdaf4a7454cc469e161e76f49160b
orlando.rubio <orlandorivasrubio@live.com>
2018-11-26 15:07:19 -0500

    Se termina todo el back-end pero da error 401

--- polls/src/main/java/com/example/polls/controller/AuthController.java
+++ polls/src/main/java/com/example/polls/controller/AuthController.java
@@ -12,7 +12,6 @@ import com.example.polls.repository.RoleRepository;
 import com.example.polls.repository.UserRepository;
 import com.example.polls.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
